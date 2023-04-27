43790ec1f94cc511cd92c267c908c41cbd7b7e6c
Edmond Chuc <edmond@kurrawong.ai>
2023-04-27 16:00:47 +1000

    Update SpacePrez home page content

--- src/views/spaceprez/SpacePrezHomeView.vue
+++ src/views/spaceprez/SpacePrezHomeView.vue
@@ -17,7 +17,9 @@ onMounted(() => {
 <template>
     <h1>Spatial Data</h1>
     <p>
-        A spatial catalog of features conforming to DCAT and GeoSPARQL, delivered in both human and machine-readable views. 
+        A spatial catalog of Feature Collections and Features conforming to <a href="https://www.w3.org/TR/vocab-dcat-2/">DCAT</a> and 
+        <a href="https://www.ogc.org/standard/geosparql/">GeoSPARQL</a>, and delivered as a read-only web system in both human and machine-readable views.
+        The views are known as profiles and a list of them are available <RouterLink to="/s/profiles">here</RouterLink>.
     </p>
     <p>
         The system provides a set of RDF APIs and also an OGC API for Feature Collections and Features.
