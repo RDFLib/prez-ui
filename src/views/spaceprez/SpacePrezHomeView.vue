4c0e243c4687fde8270acde0c0c4655eeaa9798d
Edmond Chuc <edmond@kurrawong.ai>
2023-04-27 15:47:48 +1000

    Streamline SpacePrez terminology

--- src/views/spaceprez/SpacePrezHomeView.vue
+++ src/views/spaceprez/SpacePrezHomeView.vue
@@ -2,20 +2,27 @@
 import { onMounted } from "vue";
 import { useUiStore } from "@/stores/ui";
 import SpacePrezSearchMap from "@/components/search/SpacePrezSearchMap.vue";
+import { getPrezSystemLabel } from "@/util/prezSystemLabelMapping";
 
 const ui = useUiStore();
 
 onMounted(() => {
     ui.rightNavConfig = { enabled: false };
-    document.title = "SpacePrez | Prez";
+    document.title = "Spatial Data Home | Prez";
     ui.pageHeading = { name: "SpacePrez", url: "/s"};
-    ui.breadcrumbs = [{ name: "SpacePrez", url: "/s" }];
+    ui.breadcrumbs = [{ name: getPrezSystemLabel("SpacePrez") + " Home", url: "/s" }];
 });
 </script>
 
 <template>
-    <h1>SpacePrez Home</h1>
-    <p>Welcome to SpacePrez</p>
+    <h1>Spatial Data</h1>
+    <p>
+        A spatial catalog of features conforming to DCAT and GeoSPARQL, delivered in both human and machine-readable views. 
+    </p>
+    <p>
+        The system provides a set of RDF APIs and also an OGC API for Feature Collections and Features.
+        See the <RouterLink to="/docs">API Documentation</RouterLink> for more information.
+    </p>
     <SpacePrezSearchMap />
 </template>
 
