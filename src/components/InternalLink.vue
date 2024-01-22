8699de1c71b818e924756994ba3ee9ce91606bf9
jamiefeiss <jamie.feiss@gmail.com>
2024-01-23 08:28:21 +1000

    Changed the multiple links in InternalLink to a popover menu

--- src/components/InternalLink.vue
+++ src/components/InternalLink.vue
@@ -3,6 +3,7 @@ import { computed } from "vue";
 import { RouterLink } from "vue-router";
 import type { PropTableObject } from "@/types";
 import ToolTip from "@/components/ToolTip.vue";
+import Popover from "@/components/Popover.vue";
 
 const props = defineProps<PropTableObject>();
 
@@ -15,11 +16,16 @@ const links = computed(() => {
 <template>
     <div class="link">
         <component :is="!!props.description ? ToolTip : 'slot'">
-            <RouterLink v-if="links" :to="links[0]">
+            <RouterLink v-if="links && links.length === 1" :to="links[0]">
                 <template v-if="!!props.label">{{ props.label }}</template>
                 <template v-else-if="!!props.qname">{{ props.qname }}</template>
                 <template v-else>{{ props.value }}</template>
             </RouterLink>
+            <span v-else-if="links && links.length > 1">
+                <template v-if="!!props.label">{{ props.label }}</template>
+                <template v-else-if="!!props.qname">{{ props.qname }}</template>
+                <template v-else>{{ props.value }}</template>
+            </span>
             <a
                 v-else
                 :href="props.value"
@@ -32,14 +38,20 @@ const links = computed(() => {
             </a>
             <template v-if="!!props.description" #text>{{ props.description }}</template>
         </component>
-        <span v-if="links && links.length > 1" class="multi-links">
+        <Popover v-if="links && links.length > 1" class="links-btn" title="Internal links">
+            <i class="fa-solid fa-link"></i>
+            <template #content>
+                <RouterLink v-for="link in links" :to="link" class="link-item">{{ link }}</RouterLink>
+            </template>
+        </Popover>
+        <!-- <span v-if="links && links.length > 1" class="multi-links">
             <template v-for="(link, index) in links.slice(1)">
                 <template v-if="index === 0">(</template>
                 <RouterLink :to="link" class="btn sm outline alt-internal-link"><i class="fa-solid fa-link" title="Alternative internal link"></i></RouterLink>
                 <template v-if="index < links.length - 2">,&nbsp;</template>
                 <template v-if="index === links.length - 2">)</template>
             </template>
-        </span>
+        </span> -->
         <a
             v-if="links"
             :href="props.value"
@@ -64,6 +76,19 @@ const links = computed(() => {
 
     i {
         font-size: 0.8rem;
+    }
+
+    .links-btn {
+        margin-left: 4px;
+    }
+
+    .link-item {
+        padding: 6px;
+        transition: background-color 0.2s ease-in-out;
+
+        &:hover {
+            background-color: #f3f3f3;
+        }
     }
 
     .ext-link {
