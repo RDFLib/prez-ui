9d7f043f7634b1b3de3ae2127433b79e88b7f2e8
Alejandro Villar <avillar@ogc.org>
2024-12-03 13:02:10 +0100

    Remove globalProfiles from state (SSR)
    This is to make it more compatible with SSR, avoiding
    errors like https://github.com/nuxt/nuxt/issues/20889

--- packages/core/app/site/composables/useGlobalProfiles.ts
+++ packages/core/app/site/composables/useGlobalProfiles.ts
@@ -1,7 +1,7 @@
 import { buildProfiles, dumpNodeArray, getList, getProfiles, SYSTEM_PREDICATES, type PrezBlankNode, type PrezFocusNode, type PrezNode, type PrezProfiles, type PrezProperties, type PrezProperty } from "@/base/lib";
 
 export const useGlobalProfiles = () => {
-  const profiles = useState<PrezProfiles | null>('globalProfiles', () => null);
+  const profiles = ref<PrezProfiles | null>(null);
   const runtimeConfig = useRuntimeConfig();
   const nuxtApp = useNuxtApp();
   
@@ -16,7 +16,6 @@ export const useGlobalProfiles = () => {
           if (runtimeConfig.public.prezDebug) {
             console.log("Profile Meta (debug mode)", profiles.value)
           }
-
 
         } catch (err) {
           console.error('Failed to fetch global profiles', err);
