20f1db23f067110d72fd5cd069ec3ec183da3425
Alejandro Villar <avillar@ogc.org>
2024-12-04 11:39:05 +0100

    Additional slots for ListPage

--- packages/core/app/site/components/ListPage.vue
+++ packages/core/app/site/components/ListPage.vue
@@ -31,48 +31,73 @@ watch(()=>route.fullPath, () => {
 <template>
     <NuxtLayout sidepanel>
         <template #header-text>
-            {{ header }}
+            <slot name="header-text" :data="data">
+                {{ header }}
+            </slot>
         </template>
+
         <template #debug>
             <pre class="p-2"><b>{{currentProfile?.title}}</b><br>{{ dumpNodeArray(globalProfiles?.[currentProfile?.uri || '']) }}</pre>
         </template>
+
         <template #breadcrumb>
-            <div :key="data?.parents.join()">
-                <ItemBreadcrumb v-if="data" :prepend="appConfig.breadcrumbPrepend || []" :name-substitutions="appConfig.nameSubstitutions" :parents="data.parents" />
-                <ItemBreadcrumb v-else-if="error" :custom-items="[{url: '/', label: 'Unable to load page'}]" />
-                <ItemBreadcrumb v-else :prepend="appConfig.breadcrumbPrepend" :custom-items="[{url: '#', label: '...'}]" />
-            </div>
+            <slot name="breadcrumb" :data="data">
+                <div :key="data?.parents.join()">
+                    <ItemBreadcrumb v-if="data" :prepend="appConfig.breadcrumbPrepend || []" :name-substitutions="appConfig.nameSubstitutions" :parents="data.parents" />
+                    <ItemBreadcrumb v-else-if="error" :custom-items="[{url: '/', label: 'Unable to load page'}]" />
+                    <ItemBreadcrumb v-else :prepend="appConfig.breadcrumbPrepend" :custom-items="[{url: '#', label: '...'}]" />
+                </div>
+            </slot>
         </template>
+
         <template #default>
-            <div>
-                <div v-if="error"><Message severity="error">{{ error }}</Message></div>
-                <Loading v-if="status == 'pending'" />
-                <div v-else-if="data?.data">
+            <slot :data="data" :status="status">
+
+                <slot name="top" :data="data" :status="status"></slot>
+
+                <slot name="message">
+                    <div v-if="error">
+                        <Message severity="error">{{ error }}</Message>
+                    </div>
+                </slot>
+
+                <div v-if="data?.data && status !== 'pending'">
+                    <slot name="list-top" :data="data"></slot>
                     <ItemList 
                         v-if="globalProfiles && currentProfile"
                         :fields="globalProfiles?.[currentProfile?.uri || '']"
                         :list="data.data" :key="urlPath" 
                     />
-                    <Loading v-else />
 
-                    <div class="pt-4">
-                        <Paginator
-                            v-if="data.count > pagination.limit!"
-                            :first="pagination.first" 
-                            :rows="pagination.limit" 
-                            :page="pagination.page"
-                            :totalRecords="data.count + (data.maxReached ? 1 : 0)" 
-                            @page="navigateToPage" 
-                        >
-                        </Paginator>
-                        <div v-if="data.count > 0" class="text-sm text-gray-500 text-center">
-                            Showing {{ pagination.first }} to 
-                                {{ Math.min(pagination.first! + pagination.limit! - 1, data.count) }} of 
-                                {{ data.count }}{{ data.maxReached ? '+' : '' }} items
+                    <slot name="pagination" :data="data" :pagination="pagination" :navigate-to-page="navigateToPage">
+                        <div class="pt-4">
+                            <Paginator
+                                v-if="data.count > pagination.limit!"
+                                :first="pagination.first"
+                                :rows="pagination.limit"
+                                :page="pagination.page"
+                                :totalRecords="data.count + (data.maxReached ? 1 : 0)"
+                                @page="navigateToPage"
+                            >
+                            </Paginator>
+                            <div v-if="data.count > 0" class="text-sm text-gray-500 text-center">
+                                Showing {{ pagination.first }} to
+                                    {{ Math.min(pagination.first! + pagination.limit! - 1, data.count) }} of
+                                    {{ data.count }}{{ data.maxReached ? '+' : '' }} items
+                            </div>
                         </div>
-                    </div>
+                    </slot>
+
+                    <slot name="list-bottom" :data="data"></slot>
                 </div>
-            </div>
+
+                <slot name="loading" :status="status">
+                    <Loading v-if="status == 'pending'" />
+                </slot>
+
+            </slot>
+
+            <slot name="bottom" :data="data" :status="status"></slot>
         </template>
         <template #sidepanel>
             <ItemProfiles :key="status" :apiUrl="apiUrl" :loading="status == 'pending'" :profiles="data?.profiles" />
