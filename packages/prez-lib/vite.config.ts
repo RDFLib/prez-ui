a7e3e7d964f3ef5907efb4d8a361582aecbdf22f
hjohns <howiejohns@gmail.com>
2025-04-17 16:43:51 +1000

    chore: revert vite config due

--- packages/prez-lib/vite.config.ts
+++ packages/prez-lib/vite.config.ts
@@ -5,25 +5,12 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [dts({
-        insertTypesEntry: true,
-    })],
+    plugins: [dts()],
     build: {
         lib: {
             entry: resolve(__dirname, "src/index.ts"),
-            name: "PrezLib",
-            formats: ["es", "umd"],
-            fileName: (format) => `prez-lib.${format === 'umd' ? 'umd.cjs' : 'js'}`,
-        },
-        sourcemap: true,
-        minify: false,
-        rollupOptions: {
-            external: ["n3"],
-            output: {
-                globals: {
-                    n3: "N3",
-                },
-            },
+            name: "prez-lib",
+            fileName: "prez-lib",
         },
     },
     resolve: {
