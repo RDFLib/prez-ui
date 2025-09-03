4fea3aeeebb70558b9a7fbff87435f195ab82c5e
jamiefeiss <jamie.feiss@gmail.com>
2025-09-03 13:19:11 +1000

    Updated package versions & pagination component

--- packages/prez-ui/components/PrezPagination.vue
+++ packages/prez-ui/components/PrezPagination.vue
@@ -22,7 +22,16 @@ const props = defineProps<{
 
 <template>
     <div class="flex flex-col gap-2 mt-4 pagination">
-        <Pagination v-if="props.totalItems > props.pagination.limit" v-slot="{ page }" :total="props.totalItems" :itemsPerPage="props.pagination.limit" :sibling-count="1" show-edges :page="props.pagination.page" class="paginator">
+        <Pagination
+            v-if="props.totalItems > props.pagination.limit || !props.maxReached"
+            v-slot="{ page }"
+            :total="props.totalItems"
+            :itemsPerPage="props.pagination.limit"
+            :sibling-count="1"
+            show-edges
+            :page="props.pagination.page"
+            class="paginator"
+        >
             <PaginationContent v-slot="{ items }" class="flex items-center gap-1 justify-center">
                 <PaginationFirst as-child>
                     <Button class="w-10 h-10 p-0" variant="outline" as-child>
@@ -31,13 +40,13 @@ const props = defineProps<{
                         </NuxtLink>
                     </Button>
                 </PaginationFirst>
-                <PaginationPrevious as-child>
+                <PaginationPrev as-child>
                     <Button class="w-10 h-10 p-0" variant="outline" as-child>
                         <NuxtLink :to="{...route, query: { ...route.query, page: (props.pagination.page - 1).toString() }}">
                             <ChevronLeft class="size-4" />
                         </NuxtLink>
                     </Button>
-                </PaginationPrevious>
+                </PaginationPrev>
 
                 <template v-for="(item, index) in items">
                     <PaginationItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
@@ -48,7 +57,7 @@ const props = defineProps<{
                     <PaginationEllipsis v-else :key="item.type" :index="index" />
                 </template>
 
-                <PaginationNext as-child>
+                <PaginationNext as-child :disabled="props.maxReached">
                     <Button class="w-10 h-10 p-0" variant="outline" as-child>
                         <NuxtLink :to="{...route, query: { ...route.query, page: (props.pagination.page + 1).toString() }}">
                             <ChevronRight class="size-4" />
@@ -64,10 +73,11 @@ const props = defineProps<{
                 </PaginationLast>
             </PaginationContent>
         </Pagination>
+        <PageLimitSelect :limit="props.pagination.limit" />
         <div v-if="props.totalItems > 0" class="pagination-text text-sm text-muted-foreground text-center">
             Showing {{ props.pagination.first }} to 
                 {{ Math.min(props.pagination.first + props.pagination.limit - 1, props.totalItems) }} of 
-                {{ props.totalItems }}{{ props.maxReached ? '+' : '' }} items
+                {{ props.totalItems }}{{ props.maxReached ? '' : '+' }} items
         </div>
     </div>
 </template>
